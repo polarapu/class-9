3e320018882ffd4cc43e0e3af782a59872567b27
srinivas <srinivas.reddy865@gmail.com>
2017-05-12 07:45:06 +0530

    files moved by srinivas

---
+++ first.rb
@@ -0,0 +1,26 @@
+print "this is m first ruby programe"
+
+print "this is mmy secon ruby prog...\n"
+
+puts "this is 3rd line"
+
+print "new world.."
+
+my_car = "Ferraii"
+
+print my_car
+
+puts "addition"
+
+puts 9 + 8
+
+
+age = 19
+
+if age > 16
+	puts "u r not enough to vote"
+
+else
+  puts "u r gender"
+end
+   
